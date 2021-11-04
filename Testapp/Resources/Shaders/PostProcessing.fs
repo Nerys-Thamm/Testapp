b66b61dce42a580ab7c69f877957e4924ddb6852
Nerys <NerysThamm@gmail.com>
2021-11-05 02:24:23 +1300

    Fixed the PP shader

--- Testapp/Resources/Shaders/PostProcessing.fs
+++ Testapp/Resources/Shaders/PostProcessing.fs
@@ -16,33 +16,45 @@ out vec4 FinalColor;
 void main()
 {
    
-   // distance from center of image, used to adjust blur
+    float PI = 6.28318530718; //2*PI because thats a circle
+    float Directions = 32.0; //Number of directions to sample
+    float Quality = 10.0; // Times to sample
+    float Size = 0.8; // Radius of circle to sample from surrounding pixels
+    vec2 Radius = vec2(Size, Size);
+
+   
    	vec2 uv = FragTexCoords.xy;
    	float d = length((uv) - vec2(0.5,0.5));
    	
-   	// blur amount
-   	float blur = 0.0;	
-   	blur = (1.0 + sin(CurrentTime*6.0)) * 0.5;
-   	blur *= 1.0 + sin(CurrentTime*16.0) * 0.5;
-   	blur = pow(blur, 3.0);
-   	blur *= 0.05;
-   	// reduce blur towards center
-   	blur *= d;
+   	// Chromatic abberation
+   	float Chroma = 0.0;	
+   	Chroma = (1.0 + sin(CurrentTime*6.0)) * 0.5;
+   	Chroma *= 1.0 + sin(CurrentTime*16.0) * 0.5;
+   	Chroma = pow(Chroma, 3.0);
+   	Chroma *= 0.01;
+   	Chroma *= d;
    	
    	// final color
-       vec3 col;
-       col.r = texture( ImageTexture, vec2(uv.x+blur,uv.y) ).r;
-       col.g = texture( ImageTexture, uv ).g;
-       col.b = texture( ImageTexture, vec2(uv.x-blur,uv.y) ).b;
-   	
-   	// scanline
-   	float scanline = sin(uv.y*1600.0)*0.04;
-   	col -= scanline;
-   	
-   	// vignette
-   	col -= pow((d), 3.0);
-   	
+    vec3 col;
        
+   	
+    for(float i=0.0; i<PI; i+=PI/Directions)
+    {
+        for(float j=1.0/Quality; j<=1.0; j+=1.0/Quality)
+        {
+			
+            col.r += texture( ImageTexture, (uv+vec2(cos(i),sin(i))*(Radius* pow((d), 3.0))*j)+vec2(Chroma,0.0)).r;
+            col.g += texture( ImageTexture, (uv+vec2(cos(i),sin(i))*(Radius* pow((d), 3.0))*j)+vec2(0.0,Chroma)).g;
+            col.b += texture( ImageTexture, (uv+vec2(cos(i),sin(i))*(Radius* pow((d), 3.0))*j)-vec2(Chroma,Chroma)).b;
+        }
+    }
+
+    col /= Quality * Directions - 15.0;
+   	// scanline
+    float scanline = sin(uv.y*1600.0)*0.04;
+    col -= scanline;
+   	
+ 
     //Calculate final colour
-	FinalColor = vec4(col,1.0);
+    FinalColor = vec4(col,1.0);
 }
